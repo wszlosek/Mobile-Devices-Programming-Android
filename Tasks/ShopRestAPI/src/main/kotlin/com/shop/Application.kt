69905522c128ab8a091800a4b0d1f080ac8aa558
Wojciech Szlosek <wojciech185sz@gmail.com>
2021-11-27 13:43:14 +0100

    create "get" & "post" in Product data class

--- Tasks/ShopRestAPI/src/main/kotlin/com/shop/Application.kt
+++ Tasks/ShopRestAPI/src/main/kotlin/com/shop/Application.kt
@@ -6,10 +6,25 @@ import com.shop.plugins.*
 import io.ktor.application.*
 import io.ktor.response.*
 import io.ktor.routing.*
+import java.math.BigDecimal
+
+val productStorrage = mutableListOf<Product>()
 
 fun main() {
+
+    productStorrage.addAll(
+        arrayOf(
+            Product(1, "xd", "Car", 30000, "blabla")
+        )
+    )
+
     embeddedServer(Netty, port = 8080, host = "0.0.0.0") {
         configureRouting()
         configureSerialization()
     }.start(wait = true)
 }
+
+data class Product(val id: Int, val name: String, val categoryName: String,
+                   val price: Int, val description: String)
+
+
--- Tasks/ShopRestAPI/src/main/kotlin/com/shop/plugins/Serialization.kt
+++ Tasks/ShopRestAPI/src/main/kotlin/com/shop/plugins/Serialization.kt
@@ -1,8 +1,11 @@
 package com.shop.plugins
 
+import com.shop.Product
+import com.shop.productStorrage
 import io.ktor.gson.*
 import io.ktor.features.*
 import io.ktor.application.*
+import io.ktor.http.*
 import io.ktor.response.*
 import io.ktor.request.*
 import io.ktor.routing.*
@@ -12,10 +15,25 @@ fun Application.configureSerialization() {
         gson {
         }
     }
+    productSerialization()
+}
 
+fun Application.productSerialization() {
     routing {
-        get("/json/gson") {
-            call.respond(mapOf("hello" to "world"))
+        get("/product/{id}") {
+            val id = call.parameters["id"]
+            val product: Product = productStorrage.find { it.id == id!!.toInt() }!!
+            call.respond(product)
+        }
+
+        post("/product") {
+            val product = call.receive<Product>()
+            productStorrage.add(product)
+            call.respondText("Product stored correctly", status = HttpStatusCode.Created)
+        }
+
+        put("/product/{id}") {
+            val product = call.receive<Product>()
         }
     }
 }
